2951911b204e88e89a03c6404a0776fdf951d8eb
Imani Ward <idward712@gmail.com>
2023-02-13 12:17:56 -0500

    oo

--- CardGame/ViewController.swift
+++ CardGame/ViewController.swift
@@ -20,18 +20,29 @@ class ViewController: UIViewController {
     
     @IBOutlet var image: UIImageView!
     
-    
-    
-    
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    var choice = card[0]
+    var point = 0
+    
+    func updatePoint(){
+        score.text = String("Score: \(point)")
+    }
+    
+    
+    
 
 }
+
+struct Cards{
+    var image: UIImage
+    var value: String
+}
+
+var card = [Cards(image:UIImage(named: "1")!, value: "Guess whether the next card will be higher or lower than 1"), Cards(image:UIImage(named: "2")!, value: "Guess whether the next card will be higher or lower than 2"), Cards(image:UIImage(named: "3")!, value: "Guess whether the next card will be higher or lower than 3"), ]
+
+
 
 
