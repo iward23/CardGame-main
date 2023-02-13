96d844e32836550931d8f523635a2d9851bb0b46
Imani Ward <idward712@gmail.com>
2023-02-13 11:45:35 -0500

    Initial Commit

---
+++ CardGame/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  CardGame
+//
+//  Created by Lisette Antigua on 2/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet var score: UILabel!
+    
+    @IBOutlet var guess: UILabel!
+    
+    @IBOutlet var LButton: UIButton!
+    
+    @IBOutlet var HButton: UIButton!
+    
+    @IBOutlet var image: UIImageView!
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
